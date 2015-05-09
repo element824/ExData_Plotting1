09aa31f7b47e15d7881cb88a3ed759e9ef7abfea
Koushik Nagarajan <koushik.nagarajan@gmail.com>
2015-05-09 18:22:41 +1000

    COURSE PROJECT - 1 : 	SUBMITTED.

---
+++ plot1.R
@@ -0,0 +1,11 @@
+install.packages("sqldf", dependencies = TRUE)
+library("sqldf")
+
+newdataset<- read.csv.sql("household_power_consumption.txt", sep = ";", sql = "select * from file where Date = '2/2/2007' or Date = '1/2/2007' ", eol = "\n")
+
+par(mfcol = c(1,1))
+hist(newdataset$Global_active_power,col="red" ,xlab = "Global Active Power (kilowatts)", main="Global Active Power")
+
+dev.copy(png, file = "plot1.png", height=480, width=480) 
+ 
+dev.off()
